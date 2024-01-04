846604f037ed695bcb5ba614af4a9e82aaf5507f
Виталий P15 <valexms@mail.ru>
2024-01-04 10:13:42 +0300

    Initial React App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="app">
+			<header className="app-header">
+				<h1>Book library app</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
