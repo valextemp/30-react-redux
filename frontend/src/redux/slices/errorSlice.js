f5a73115cf12cad5c52f1c29817d1f1d36339307
Виталий P15 <valexms@mail.ru>
2024-01-05 19:46:22 +0300

    Added errorSlice

---
+++ frontend/src/redux/slices/errorSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = "";
+const errorSlice = createSlice({
+	name: "error",
+	initialState: initialState,
+	reducers: {
+		setError: (state, action) => {
+			return action.payload;
+		},
+		clearError: () => {
+			return initialState;
+		},
+	},
+});
+
+export const { setError, clearError } = errorSlice.actions;
+
+export const selectErrorMessage = (state) => state.errorSlice;
+
+export default errorSlice.reducer;
